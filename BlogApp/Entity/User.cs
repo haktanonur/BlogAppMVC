b1c24015aea01b8da34f22b795f2fa4fceeeb116
Onur Haktan <onurhaktan243@gmail.com>
2023-12-07 14:51:14 +0300

    create entities

---
+++ BlogApp/Entity/User.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Entity;
+
+public class User
+{
+    public int UserId { get; set; } 
+    public string? UserName { get; set; }
+    public List<Post> Posts { get; set; } = new List<Post>();
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+}
--- Entity/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Entity;
-
-public class Class1
-{
-
-}
