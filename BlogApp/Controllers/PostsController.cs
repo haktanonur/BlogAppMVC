7d16b6ed47ec93940e540c7e99dd502254cfa22e
Onur Haktan <onurhaktan243@gmail.com>
2023-12-10 23:51:31 +0300

    tag list in details page

--- BlogApp/Controllers/PostsController.cs
+++ BlogApp/Controllers/PostsController.cs
@@ -29,7 +29,10 @@ namespace BlogApp.Controllers
 
         public async Task<IActionResult> Details(string? url)
         {
-            return View(await _postRepository.Posts.FirstOrDefaultAsync(p => p.Url == url));
+            return View(await _postRepository
+                                .Posts
+                                .Include(x => x.Tags)
+                                .FirstOrDefaultAsync(p => p.Url == url));
         }
     }
 }
---
+++ BlogApp/Migrations/20231210203319_UpdateColumnTagColorr.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateColumnTagColorr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Color",
+                table: "Tags",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Tags");
+        }
+    }
+}
