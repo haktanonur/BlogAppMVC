4fd3bd11a92154212ebb59317d0c18cd4354a9b1
Onur Haktan <onurhaktan243@gmail.com>
2023-12-10 22:20:42 +0300

    add newPost component

--- BlogApp/ViewComponents/TagsMenu.cs
+++ BlogApp/ViewComponents/TagsMenu.cs
@@ -1,5 +1,6 @@
 using BlogApp.Data.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BlogApp.ViewComponents
@@ -12,9 +13,9 @@ namespace BlogApp.ViewComponents
             _tagRepository = tagRepository;
         }
 
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View(_tagRepository.Tags.ToList());
+            return View(await _tagRepository.Tags.ToListAsync());
         }
 
     }
